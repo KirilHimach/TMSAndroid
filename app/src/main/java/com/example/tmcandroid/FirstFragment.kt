4954cbb7575356732b2facea620cacc2978a6aef
Kiril Himach <himachkiril@gmail.com>
2023-04-11 18:59:47 +0300

    Task 3 Himach

---
+++ app/src/main/java/com/example/tmcandroid/FirstFragment.kt
@@ -0,0 +1,42 @@
+package com.example.tmcandroid
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
+import androidx.navigation.fragment.findNavController
+import com.example.tmcandroid.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        goToSecondFragment()
+    }
+
+    private fun goToSecondFragment() {
+        binding.passwordEditText.doOnTextChanged { text1, start1, before1, count1 ->
+            binding.logInButton.isEnabled = text1!!.length >= 8
+        }
+        binding.logInButton.setOnClickListener {
+            if (binding.usernameEditText.length() > 0) {
+                findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            }
+        }
+    }
+}
