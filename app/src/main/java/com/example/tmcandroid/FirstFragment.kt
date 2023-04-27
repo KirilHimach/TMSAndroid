d0d386ee4746eec5b729a747b379d297f3c0206e
Kiril <himachkiril@gmail.com>
2023-04-27 16:45:21 +0300

    Task 6 Himach

---
+++ app/src/main/java/com/example/tmcandroid/FirstFragment.kt
@@ -0,0 +1,42 @@
+package com.example.tmcandroid
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.tmcandroid.databinding.FragmentFirstBinding
+
+
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            onNextFragment()
+        }
+    }
+
+    private fun onNextFragment() {
+        parentFragmentManager
+            .beginTransaction()
+            .replace(R.id.main_fragment_container, SecondFragment())
+            .commit()
+    }
+
+}
--- app/src/main/java/com/example/tmcandroid/MainActivity.kt
+++ app/src/main/java/com/example/tmcandroid/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.tmcandroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.tmcandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.toFirst.setOnClickListener {
+            transaction(FirstFragment())
+        }
+
+        binding.toSecond.setOnClickListener {
+            transaction(SecondFragment())
+        }
+    }
+
+    private fun transaction(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.main_fragment_container, fragment)
+            .commit()
     }
 }
